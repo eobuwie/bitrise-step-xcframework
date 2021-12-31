91d28e3630cd5fd8b6a347a7c4e6e13f59b8da25
Norbert Sroczyński <norbert.sroczynski@eobuwie.com.pl>
2021-12-31 19:16:21 +0100

    Added ability to choose platforms

--- step.sh
+++ step.sh
@@ -21,55 +21,76 @@ function bitcodeSymbols {
   UDIDs=$(dwarfdump --uuid $2 | cut -d ' ' -f2)
   SYMBOLS=""
   for udid in $UDIDs; do
-    PATH=$(pwd)/archives/$NAME/$1/$NAME.xcarchive/BCSymbolMaps/$udid.bcsymbolmap
-    SYMBOLS="$SYMBOLS -debug-symbols $PATH "
+    BCSYMBOLMAP_PATH=$(pwd)/archives/$NAME/$1/$NAME.xcarchive/BCSymbolMaps/$udid.bcsymbolmap
+    if [[ -f "$BCYMBOLMAP_PATH" ]]; then
+      SYMBOLS="$SYMBOLS -debug-symbols $BCSYMBOLMAP_PATH "
+    fi
   done
   echo $SYMBOLS
 }
 
+params=()
+
 # macOS
 
-archive $WORKSPACE $NAME-macos "generic/platform=macOS" "archives/$NAME/macOS/$NAME"
-archive $WORKSPACE $NAME-ios "generic/platform=macOS,variant=Mac Catalyst" "archives/$NAME/macOS Catalyst/$NAME"
+if [ "$macos" = "yes" ]; then
+  archive $WORKSPACE $NAME-macos "generic/platform=macOS" "archives/$NAME/macOS/$NAME"
+
+  params+=(-framework "archives/$NAME/macOS/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework")
+  params+=(-debug-symbols "$(pwd)/archives/$NAME/macOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM")
+fi
+
+# macOS Catalyst
+
+if [ "$macoscatalyst" = "yes" ]; then
+  archive $WORKSPACE $NAME-ios "generic/platform=macOS,variant=Mac Catalyst" "archives/$NAME/macOS Catalyst/$NAME"
+
+  params+=(-framework "archives/$NAME/macOS Catalyst/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework")
+  params+=(-debug-symbols "$(pwd)/archives/$NAME/macOS Catalyst/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM")
+fi
 
 # iOS
 
-archive $WORKSPACE $NAME-ios "generic/platform=iOS" "archives/$NAME/iOS/$NAME"
-archive $WORKSPACE $NAME-ios "generic/platform=iOS Simulator" "archives/$NAME/iOS Simulator/$NAME"
+if [ "$ios" = "yes" ]; then
+  archive $WORKSPACE $NAME-ios "generic/platform=iOS" "archives/$NAME/iOS/$NAME"
+  archive $WORKSPACE $NAME-ios "generic/platform=iOS Simulator" "archives/$NAME/iOS Simulator/$NAME"
+
+  params+=(-framework "archives/$NAME/iOS/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework")
+  params+=(-debug-symbols "$(pwd)/archives/$NAME/iOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM")
+  params+=($(bitcodeSymbols "iOS" "$(pwd)/archives/$NAME/iOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM"))
+  params+=(-framework "archives/$NAME/iOS Simulator/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework")
+  params+=(-debug-symbols "$(pwd)/archives/$NAME/iOS Simulator/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM")
+fi
 
 # watchOS
 
-archive $WORKSPACE $NAME-watchos "generic/platform=watchOS" "archives/$NAME/watchOS/$NAME"
-archive $WORKSPACE $NAME-watchos "generic/platform=watchOS Simulator" "archives/$NAME/watchOS Simulator/$NAME"
+if [ "$watchos" = "yes" ]; then
+  archive $WORKSPACE $NAME-watchos "generic/platform=watchOS" "archives/$NAME/watchOS/$NAME"
+  archive $WORKSPACE $NAME-watchos "generic/platform=watchOS Simulator" "archives/$NAME/watchOS Simulator/$NAME"
+
+  params+=(-framework "archives/$NAME/watchOS/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework")
+  params+=(-debug-symbols "$(pwd)/archives/$NAME/watchOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM")
+  params+=($(bitcodeSymbols "watchOS" "$(pwd)/archives/$NAME/watchOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM"))
+  params+=(-framework "archives/$NAME/watchOS Simulator/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework")
+  params+=(-debug-symbols "$(pwd)/archives/$NAME/watchOS Simulator/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM")
+fi
 
 # tvOS
 
-archive $WORKSPACE $NAME-tvos "generic/platform=tvOS" "archives/$NAME/tvOS/$NAME"
-archive $WORKSPACE $NAME-tvos "generic/platform=tvOS Simulator" "archives/$NAME/tvOS Simulator/$NAME"
+if [ "$tvos" = "yes" ]; then
+  archive $WORKSPACE $NAME-tvos "generic/platform=tvOS" "archives/$NAME/tvOS/$NAME"
+  archive $WORKSPACE $NAME-tvos "generic/platform=tvOS Simulator" "archives/$NAME/tvOS Simulator/$NAME"
+
+  params+=(-framework "archives/$NAME/tvOS/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework")
+  params+=(-debug-symbols "$(pwd)/archives/$NAME/tvOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM")
+  params+=($(bitcodeSymbols "tvOS" "$(pwd)/archives/$NAME/tvOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM"))
+  params+=(-framework "archives/$NAME/tvOS Simulator/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework")
+  params+=(-debug-symbols "$(pwd)/archives/$NAME/tvOS Simulator/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM")
+fi
 
 # xcframework
 
-xcodebuild -create-xcframework \
--framework "archives/$NAME/macOS/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework" \
--debug-symbols "$(pwd)/archives/$NAME/macOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM" \
--framework "archives/$NAME/macOS Catalyst/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework" \
--debug-symbols "$(pwd)/archives/$NAME/macOS Catalyst/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM" \
--framework "archives/$NAME/iOS/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework" \
--debug-symbols "$(pwd)/archives/$NAME/iOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM" \
-$(bitcodeSymbols "iOS" "$(pwd)/archives/$NAME/iOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM") \
--framework "archives/$NAME/iOS Simulator/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework" \
--debug-symbols "$(pwd)/archives/$NAME/iOS Simulator/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM" \
--framework "archives/$NAME/watchOS/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework" \
--debug-symbols "$(pwd)/archives/$NAME/watchOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM" \
-$(bitcodeSymbols "watchOS" "$(pwd)/archives/$NAME/watchOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM") \
--framework "archives/$NAME/watchOS Simulator/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework" \
--debug-symbols "$(pwd)/archives/$NAME/watchOS Simulator/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM" \
--framework "archives/$NAME/tvOS/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework" \
--debug-symbols "$(pwd)/archives/$NAME/tvOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM" \
-$(bitcodeSymbols "tvOS" "$(pwd)/archives/$NAME/tvOS/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM") \
--framework "archives/$NAME/tvOS Simulator/$NAME.xcarchive/Products/Library/Frameworks/$NAME.framework" \
--debug-symbols "$(pwd)/archives/$NAME/tvOS Simulator/$NAME.xcarchive/dSYMs/$NAME.framework.dSYM" \
--output "archives/$NAME.xcframework"
+xcodebuild -create-xcframework "${params[@]}" -output "archives/$NAME.xcframework"
 
 # fix xcframework
 find "archives/$NAME.xcframework" -name "*.swiftinterface" -exec sed -i -e "s/${NAME}\.//g" {} \;
